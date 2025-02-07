d3ac0da2a3b8316cf903ab2604cf963cf08b904a
Kuldeep Yeware <kuldeepyeware31@gmail.com>
2025-02-07 14:44:17 +0530

    Base Repo with Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,14 @@
-import { Stack, Link } from 'expo-router';
+import { Text, View } from "react-native";
 
-import { Button } from '~/components/Button';
-import { Container } from '~/components/Container';
-import { ScreenContent } from '~/components/ScreenContent';
-
-export default function Home() {
+export default function Index() {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Home' }} />
-      <Container>
-        <ScreenContent path="app/index.tsx" title="Home" />
-        <Link href={{ pathname: '/details', params: { name: 'Dan' } }} asChild>
-          <Button title="Show Details" />
-        </Link>
-      </Container>
-    </>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
   );
 }
